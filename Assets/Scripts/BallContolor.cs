a20bf20da745efb76a36d61a64d0e9fc77d223a7
shariful islam <sisagor98@gmail.com>
2019-10-23 20:33:31 +0600

    3Level complate

---
+++ Assets/Scripts/BallContolor.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.SceneManager;
+using UnityEngine.UI;
+
+public class BallContolor : MonoBehaviour
+{
+    public float speed=150f;
+    public Rigidbody rb;
+    private Vector2 lastMousePosition;
+
+    public GameObject Endgame;
+    public GameObject win;
+    private float wallDistance = 4.5f;
+    public float minCameraDistance = 3f;
+    private int lavel=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Endgame.SetActive(false);
+        win.SetActive(false);
+       // rb.AddForce(force);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 deltaposition = Vector2.zero;
+        if(Input.GetMouseButton(0)){
+            Vector2 currentMousePosition = Input.mousePosition;
+            if(lastMousePosition == Vector2.zero){
+                lastMousePosition = currentMousePosition;
+            }
+            deltaposition= currentMousePosition -lastMousePosition;
+            lastMousePosition = currentMousePosition;
+            Vector3 force = new Vector3(deltaposition.x,0, deltaposition.y)*speed ;
+            rb.AddForce(force);
+            }
+            else {
+                lastMousePosition = Vector2.zero;
+            }
+        }
+
+    private void FixedUpdate() {
+        Vector3 pos = transform.position;
+        if(transform.position.x <-wallDistance){
+            pos.x = - wallDistance;
+
+        }
+        else if(transform.position.x > wallDistance){ pos.x = wallDistance;
+        }
+
+        if(transform.position.z < Camera.main.transform.position.z+minCameraDistance)
+        {
+            pos.z =Camera.main.transform.position.z+minCameraDistance;
+        } transform.position =pos;
+
+    }
+    
+    private int count=0;
+    private void OnCollisionEnter(Collision collision) {
+        
+        if(collision.gameObject.tag == "Enemy")
+        {
+            count++;
+            Debug.Log(count);
+            if(count>=5)
+            {
+                
+                Endgame.SetActive (true);
+                //Destroy(this);
+            }
+        }
+        if(collision.gameObject.tag == "win"){
+            win.SetActive (true);
+        }
+    } 
+    public void RestartGame(int level){
+        UnityEngine.SceneManagement.SceneManager.LoadScene(level);
+    }  
+
+    public void NextLevel(int level){
+        level=level+ 1;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(level);
+    }   
+}
+
---
+++ Assets/Scripts/CameraContoller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraContoller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform Player;
+    private Vector3 offset;
+
+    public bool LookAtPlayer = false;
+
+    [Range(0.01f,1f)]
+    public float Smootfactor = 0.5f;
+
+    void Start()
+    {
+        offset = transform.position - Player.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPos = Player.position + offset;
+        transform.position = Vector3.Slerp(transform.position, newPos,Smootfactor * Time.deltaTime);
+        if(LookAtPlayer)
+        {
+            transform.LookAt(Player);
+        }
+    }
+}
